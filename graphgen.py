8b144aa4dd410a0eabb5c3154504937f334a9779
Brijesh <brijesh.chaurasia@onmyowntechnology.com>
2019-10-04 11:26:57 +0530

    finding maximum value of Ex EYEz

--- graphgen.py
+++ graphgen.py
@@ -6,16 +6,21 @@ from statistics import mean
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import cm
+import math
 
 
-
-data=read_csv("records (2).csv").values.tolist()
+#getting AND storing the data
+data=read_csv("records1.csv").values.tolist()
 counter=0
-for i in range(len(data)):
-    data[i][17]=counter
-    if data[i][16]==3 and data[i+1][16]==0:
-        counter=counter+1
-        print(counter)
+try:
+    for i in range(len(data)):
+        data[i][17]=counter
+
+        if data[i][16]==3 and data[i+1][16]==0:
+            counter=counter+1
+            print(counter)
+except:
+    print("Exception occur")
 #taking the fifteen and sixteen column
 # print(data)
 
@@ -63,142 +68,139 @@ accy_final=[]
 accz_final=[]
 j=1
 
-def plottingmap(time_final3,ex_final,ey_final,ez_final,accx_final,accy_final,accz_final,sixty_percent_final):
+def plottingmap(time_final3,ex_final,ey_final,ez_final,accx_final,accy_final,accz_final,sixty_percent_final,counter):
     print("@@@@@@@@@@@@@@@@@@####################")
+    # max_ex_final=max(ex_final)
+    # print("max ex value")
+    # print(max_ex_final)
 
     plt.subplot(311)
     color=iter(cm.rainbow(np.linspace(0,1,10)))
-    for i in range(max3-2):
+    for i in range(counter-1):
         c=next(color)
         plt.plot(time_final3[i], ex_final[i],'.-',c=c,label="cycle"+str(i+1))
         # for()
         x=[]
         y_sixty=[]
-        for j in range(100):
+
+        #finding max of the each set for plotting a 60 percent line
+        max_ex_final=max(ex_final[i])
+        print(math.ceil(max_ex_final))
+        for j in range(math.ceil(max_ex_final)):
             y_sixty.append(j)
 
-        for j in range(100):
+        for j in range(math.ceil(max_ex_final)):
             x.append(sixty_percent_final[i])
 
         plt.plot(x,y_sixty,':',c=c)
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-        plt.title('time (s)')
+        plt.title('time (s) in percent')
         plt.ylabel('ex')
 
 
     plt.subplot(312)
     color=iter(cm.rainbow(np.linspace(0,1,10)))
-    for i in range(max3-2):
+    for i in range(counter-1):
         c=next(color)
         plt.plot(time_final3[i], ey_final[i],'.-',c=c,label="cycle"+str(i+1))
         # for()
         x=[]
         y_sixty=[]
-        for j in range(100):
+        #finding max of the each set for plotting a 60 percent line
+        max_ey_final=max(ey_final[i])
+        print(math.ceil(max_ey_final))
+        for j in range(math.ceil(max_ey_final)):
             y_sixty.append(j)
 
-        for j in range(100):
+        for j in range(math.ceil(max_ey_final)):
             x.append(sixty_percent_final[i])
 
         plt.plot(x,y_sixty,':',c=c)
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-        plt.title('time (s)')
+        plt.title('time (s) in percent')
         plt.ylabel('ey')
 
     plt.subplot(313)
     color=iter(cm.rainbow(np.linspace(0,1,10)))
-    for i in range(max3-2):
+    for i in range(counter-1):
         c=next(color)
         plt.plot(time_final3[i], ez_final[i],'.-',c=c,label="cycle"+str(i+1))
         # for()
         x=[]
         y_sixty=[]
-        for j in range(100):
+        #finding max of the each set for plotting a 60 percent line
+        max_ez_final=max(ez_final[i])
+        print(math.ceil(max_ez_final))
+        for j in range(math.ceil(max_ez_final)):
             y_sixty.append(j)
 
-        for j in range(100):
+        for j in range(math.ceil(max_ez_final)):
             x.append(sixty_percent_final[i])
 
         plt.plot(x,y_sixty,':',c=c)
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-        plt.title('time (s)')
+        plt.title('time (s) in percent')
         plt.ylabel('ez')
-
-
-
-
-
-    # plt.subplot(312)
-    # for i in range(max3-2):
-    #     plt.plot(time_final3[i], ey_final[i], '.-')
-    #     plt.title('time (s)')
-    #     plt.ylabel('ey')
-    #
-    #
-    # plt.subplot(313)
-    # for i in range(max3-2):
-    #     plt.plot(time_final3[i], ez_final[i], '.-')
-    #     plt.title('time (s)')
-    #     plt.ylabel('ez')
-
     plt.show()
 
 
 
-print("XXXXXXXXXXXXXXXXXXXXX")
-max2=[]
-for i in range(len(data)):
-    max2.append(data[i][-1])
-# print(max(max2))
-max3=max(max2)
+# print("XXXXXXXXXXXXXXXXXXXXX MAX cycle")
+# max2=[]
+# for i in range(len(data)):
+#     max2.append(data[i][-1])
+# max3=max(max2)
+# print(max3)
+
 sixty_percent=[]
-for i in range(len(data)):
-    if data[i][17]==j:
-        if data[i][16]==2 and data[i+1][16]==3:
-            ####finding a value of phase for 60%
 
-            sixty_percent.append(data[i+1][0])
+try:
+    for i in range(len(data)):
+        if data[i][17]==j:
+            if data[i][16]==2 and data[i+1][16]==3:
+                ####finding a value of phase for 60%
+                sixty_percent.append(data[i+1][0])
+            if(j>=counter):
+                break
+            elif data[i][17]==j and data[i+1][17]==j+1:
 
-        if(j==max3-1):
-            break
-        elif data[i][17]==j and data[i+1][17]==j+1:
-
-            time_final.append(time)
-            ex_final.append(ex)
-            ey_final.append(ey)
-            ez_final.append(ez)
-            accx_final.append(accx)
-            accy_final.append(accy)
-            accz_final.append(accz)
-            time=[]
-            ex=[]
-            ey=[]
-            ez=[]
-            accx=[]
-            accy=[]
-            accz=[]
-            # print("j:")
-            # print(j)
-            j+=1
-        # a2=data[i][0]
-        # b2=len(data)
-        time.append((data[i][0]))
-        ex.append(data[i][1])
-        ey.append(data[i][2])
-        ez.append(data[i][3])
-        accx.append(data[i][4])
-        accy.append(data[i][5])
-        accz.append(data[i][6])
-
+                time_final.append(time)
+                ex_final.append(ex)
+                ey_final.append(ey)
+                ez_final.append(ez)
+                accx_final.append(accx)
+                accy_final.append(accy)
+                accz_final.append(accz)
+                time=[]
+                ex=[]
+                ey=[]
+                ez=[]
+                accx=[]
+                accy=[]
+                accz=[]
+                j+=1
+            # a2=data[i][0]
+            # b2=len(data)
+            time.append((data[i][0]))
+            ex.append(data[i][1])
+            ey.append(data[i][2])
+            ez.append(data[i][3])
+            accx.append(data[i][4])
+            accy.append(data[i][5])
+            accz.append(data[i][6])
+except:
+    print("Exception2 occur")
 
 
 print("########")
 ##########converting time into percentage
-
+##time final is the list of the list in data
 
 time_final3=[]
-print(range(len(time_final)))
+print("XXXXXXXXXXXLENGTH OF TIME")
+print(len(time_final))
 sixty_percent_final=[]
+print("XXXXXXXXXXXLENGTH OF TIME")
 
 for i in range(len(time_final)):
     print(i)
@@ -207,17 +209,13 @@ for i in range(len(time_final)):
         time_final2.append((j/len(time_final[i]))*100)
         for k in range(len(sixty_percent)):
             if(time_final[i][j]==sixty_percent[k]):
-                # print("KKKKKKKKKKKKkk")
                 # print(sixty_percent[k])
                 sixty_percent_final.append((j/len(time_final[i]))*100)
-
-
-
     time_final3.append(time_final2)
 
 print(sixty_percent_final)
 
-plottingmap(time_final3,ex_final,ey_final,ez_final,accx_final,accy_final,accz_final,sixty_percent_final)
+plottingmap(time_final3,ex_final,ey_final,ez_final,accx_final,accy_final,accz_final,sixty_percent_final,counter)
 # plottingmap(time_final3,ey_final,"ey")
 # plottingmap(time_final3,ez_final,"ez")
 # plottingmap(time_final3,accx_final)
